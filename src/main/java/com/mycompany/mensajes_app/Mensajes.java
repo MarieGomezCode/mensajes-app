960dce32e48702e0a77ee1b72656d9f8fd0219c0
angela <espitiaangela740@gmail.com>
2023-11-12 12:31:48 -0500

    Añadido un nuevo archivo

---
+++ src/main/java/com/mycompany/mensajes_app/Mensajes.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mensajes_app;
+
+/**
+ *
+ * @author angela
+ */
+public class Mensajes {
+    int id_mensasaje;
+    String mensaje;
+    String autor_mensaje;
+    String fecha_mensaje;
+    public Mensajes(){}
+
+    public Mensajes(String mensaje, String autor_mensaje, String fecha_mensaje) {
+        this.mensaje = mensaje;
+        this.autor_mensaje = autor_mensaje;
+        this.fecha_mensaje = fecha_mensaje;
+    }
+    
+
+    public int getId_mensasaje() {
+        return id_mensasaje;
+    }
+
+    public void setId_mensasaje(int id_mensasaje) {
+        this.id_mensasaje = id_mensasaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getAutor_mensaje() {
+        return autor_mensaje;
+    }
+
+    public void setAutor_mensaje(String autor_mensaje) {
+        this.autor_mensaje = autor_mensaje;
+    }
+
+    public String getFecha_mensaje() {
+        return fecha_mensaje;
+    }
+
+    public void setFecha_mensaje(String fecha_mensaje) {
+        this.fecha_mensaje = fecha_mensaje;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/mensajes_app/MensajesDAO.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mensajes_app;
+
+/**
+ *
+ * @author angela
+ */
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+public class MensajesDAO {
+
+ public static void crearMensajeDB(Mensajes mensaje){
+  Conexion db_connect = new Conexion();
+
+  try(Connection conexion = db_connect.get_connection())  {
+   PreparedStatement  ps=null;
+   try{
+    String query="INSERT INTO mensajes ( mensaje, autor_mensaje) VALUES (?,?)";
+    ps=conexion.prepareStatement(query);
+    ps.setString(1, mensaje.getMensaje());
+    ps.setString(2, mensaje.getAutor_mensaje());
+    ps.executeUpdate();
+    System.out.println("mensaje creado");
+
+   }catch(SQLException ex){
+    System.out.println(ex);
+   }
+  }catch(SQLException e){
+   System.out.println(e);
+  }
+ }
+
+ public static void leerMensajesDB(){
+
+ }
+
+ public static void borrarMensajeDB(int id_mensaje){
+
+ }
+
+ public static void actualizarMensajeDB(Mensajes mensaje){
+
+ }
+
+    
+}
