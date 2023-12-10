3d4bcec9986b3935180707dbdbf38f10ec95a06c
angela <espitiaangela740@gmail.com>
2023-12-10 15:43:26 -0500

    subiendo archivos

--- src/main/java/com/mycompany/mensajes_app/MensajesDAO.java
+++ src/main/java/com/mycompany/mensajes_app/MensajesDAO.java
@@ -11,6 +11,7 @@ package com.mycompany.mensajes_app;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 public class MensajesDAO {
 
@@ -36,15 +37,74 @@ public class MensajesDAO {
  }
 
  public static void leerMensajesDB(){
+  Conexion db_connect = new Conexion();
 
+  PreparedStatement ps=null;
+  ResultSet rs=null;
+
+  try(Connection conexion = db_connect.get_connection())  {
+   String query="SELECT * FROM mensajes";
+   ps=conexion.prepareStatement(query);
+   rs=ps.executeQuery();
+
+   while(rs.next()){
+    System.out.println("ID: "+rs.getInt("id_mensaje"));
+    System.out.println("Mensaje: "+rs.getString("mensaje"));
+    System.out.println("Autor: "+rs.getString("autor_mensaje"));
+    System.out.println("Fecha: "+rs.getString("fecha_mensaje"));
+    System.out.println("");
+   }
+  }catch(SQLException e){
+   System.out.println("no se pudieron recuperar los mensajes");
+   System.out.println(e);
+  }
  }
 
+
  public static void borrarMensajeDB(int id_mensaje){
+  Conexion db_connect = new Conexion();
+
+  try(Connection conexion = db_connect.get_connection())  {
+   PreparedStatement ps=null;
+
+   try {
+    String query="DELETE FROM mensajes WHERE id_mensaje = ?";
+    ps=conexion.prepareStatement(query);
+    ps.setInt(1, id_mensaje);
+    ps.executeUpdate();
+    System.out.println("el mensaje ha sido borrado");
+   }catch(SQLException e) {
+    System.out.println(e);
+    System.out.println("no se pudo borrar el mensaje");
+   }
+
+
+  }catch(SQLException e){
+   System.out.println(e);
+  }
 
  }
 
  public static void actualizarMensajeDB(Mensajes mensaje){
+  Conexion db_connect = new Conexion();
 
+  try(Connection conexion = db_connect.get_connection())  {
+   PreparedStatement ps=null;
+
+   try{
+    String query="UPDATE mensajes SET mensaje = ? WHERE id_mensaje = ?";
+    ps=conexion.prepareStatement(query);
+    ps.setString(1, mensaje.getMensaje());
+    ps.setInt(2, mensaje.getId_mensasaje());
+    ps.executeUpdate();
+    System.out.println("mensaje se actualizó correctamente");
+   }catch(SQLException e){
+    System.out.println(e);
+    System.out.println("no se pudo actualizar el mensaje");
+   }
+  }catch(SQLException e){
+   System.out.println(e);
+  }
  }
 
     
--- src/main/java/com/mycompany/mensajes_app/mensajesService.java
+++ src/main/java/com/mycompany/mensajes_app/mensajesService.java
@@ -3,25 +3,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.mensajes_app;
-
+import java.util.Scanner;
 /**
  *
  * @author angela
  */
 public class mensajesService {
      public static void crearMensaje(){
+         Scanner sc = new Scanner(System.in);
+
+         System.out.println("Escribe tu mensaje");
+         String mensaje = sc.nextLine();
+
+         System.out.println("tu nombre");
+         String nombre = sc.nextLine();
+
+         Mensajes registro = new Mensajes();
+         registro.setMensaje(mensaje);
+         registro.setAutor_mensaje(nombre);
+         MensajesDAO.crearMensajeDB(registro);
         
     }
     
     public static void listarMensajes(){
-        
+        MensajesDAO.leerMensajesDB();
     }
     public static void borrarMensaje(){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("indica el ID del mensaje a borrar");
+        int id_mensaje= sc.nextInt();
+        MensajesDAO.borrarMensajeDB(id_mensaje);
     }
     
     public static void editarMensaje(){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escribe tu nuevo mensaje");
+        String mensaje = sc.nextLine();
+        sc.skip("");
+        System.out.println("indica el ID del mensaje a editar");
+        int id_mensaje= sc.nextInt();
+        Mensajes actualizacion = new Mensajes();
+        actualizacion.setId_mensasaje(id_mensaje);
+        actualizacion.setMensaje(mensaje);
+        MensajesDAO.actualizarMensajeDB(actualizacion);
     } 
     
 }
